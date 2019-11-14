0a9211b4927d08806acd0982d469eb7a2066a3a3
moto3ki <moto3ki@gmail.com>
2019-11-14 22:10:09 +0900

    initial

---
+++ README.md
@@ -0,0 +1,41 @@
+# chat-space DB設計
+## usersテーブル
+|Column|Type|Options|
+|------|----|-------|
+|name|string|null: false|
+|email|string|null: false|
+|password|string|null: false|
+### Association
+- has_many :comments
+- has_many :users_groups
+- has_many  :groups,  through:  :users_groups
+
+## groupsテーブル
+|Column|Type|Options|
+|------|----|-------|
+|name|string|null: false|
+### Association
+- has_many :comments
+- has_many :users_groups
+- has_many  :users,  through:  :users_groups
+
+## users_groupsテーブル
+|Column|Type|Options|
+|------|----|-------|
+|user_id|integer|null: false, foreign_key: true|
+|group_id|integer|null: false, foreign_key: true|
+### Association
+- belongs_to :user
+- belongs_to :group
+
+## commentsテーブル
+|Column|Type|Options|
+|------|----|-------|
+|text|text||
+|image|text||
+|user_id|integer|null: false, foreign_key: true|
+|group_id|integer|null: false, foreign_key: true|
+### Association
+- belongs_to :user
+- belongs_to :group
+
---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,73 @@
+$(document).on('turbolinks:load', function() {
+$(function(){ 
+  function buildHTML(message){ 
+      var image = ( message.image ) ? `<img class= "lower-message__image" src=${message.image} >` : "";
+      var html =
+       `<div class="message" data-id='${message.id}'>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.date}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          ${image} 
+        </div>`
+      return html;
+  }
+$('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.messages').append(html);
+    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    $('form')[0].reset();
+  })
+  .fail(function(){
+    alert('error');
+  });
+  return false;
+});
+
+var reloadMessages = function() {
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){
+  last_message_id = $('.message:last').data("id");
+  $.ajax({
+    url: 'api/messages',
+    type: 'GET',
+    dataType: 'json',
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    messages.forEach(function (message) {
+    var insertHTML = buildHTML(message);
+    $('.messages').append(insertHTML);
+    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+  })
+  })
+  .fail(function() {
+    alert('error');
+  });
+  };
+}
+setInterval(reloadMessages, 5000);
+});
+});
+
+
